5cb6a7bbd3c0e5451e969d2db9eb806558062a5e
CoteBoostee <anomalielive@gmail.com>
2026-07-23 12:51:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
     API_URL:
-    "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQyjHKWuXbEI8AGcRhZTOqE7QIAVFk_mEE1O4I37nEXYCBqiDNEYV3lmodW2lh9Z_JByB4q-_P8PXcDC2oyPyiAnXW1STpEnzjja_3eGlopwnUIGsZN_VXfoNYLjH4jQIokTm5IF4UUHuydx5hQBEhpOFDKkKo3Ix1mEeMN7oqI7WtCAXvrS-ArBCA1TxnlebPGPpxIf3GPiPiGros8gtvRALUgwvX3MUc9ERKREnfGlOc_6JN5GiMf62Bwa7CgwbsNcDO6JsyOB_4xxPdQcgPxxYAkxg&lib=M_6fIDV6HX3RO7FkMQl73puQ11_QnxWca",
+    "https://script.google.com/macros/s/AKfycby_EojmIDdnA7dMkUhZPH78xQ0lbnJukuV_xz5rlQnUEHlgZrXwXNBo5LzZDy1-UAdl0w/exec",
 
     SITE_NAME:
     "Mes Côtes Boostées"
