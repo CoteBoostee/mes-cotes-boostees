1ab7dc5d38070eb1a9fbbcab56c826ee05dd6e18
CoteBoostee <anomalielive@gmail.com>
2026-07-23 17:32:17 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -61,11 +61,9 @@ await fetch(
 "https://script.google.com/macros/s/AKfycby_EojmIDdnA7dMkUhZPH78xQ0lbnJukuV_xz5rlQnUEHlgZrXwXNBo5LzZDy1-UAdl0w/exec",
 
 {
-
 method:"POST",
-
+mode:"no-cors",
 body:data
-
 }
 
 );
